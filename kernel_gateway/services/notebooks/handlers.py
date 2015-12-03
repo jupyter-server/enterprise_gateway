e83c9b007e3b3f51e2fa3051778d734506542b7a
Corey A. Stubbs <cstubbs@us.ibm.com>
2015-12-03 12:52:01 -0600

    [Issue 29]: Fixed processing of Jupyter messages to properly wait for execution to complete. (c) Copyright IBM Corp. 2015

--- kernel_gateway/services/notebooks/handlers.py
+++ kernel_gateway/services/notebooks/handlers.py
@@ -12,7 +12,7 @@ except ImportError:
 class NotebookAPIHandler(tornado.web.RequestHandler):
     kernel_client = None
     sources = None
-    execution_timeout = 5
+    execution_timeout = 60
     kernel_name = ''
     _assignment_statements = {'r': "REQUEST <- '{}'",
         None : "REQUEST = '{}'"}
@@ -29,29 +29,46 @@ class NotebookAPIHandler(tornado.web.RequestHandler):
             statement = self._assignment_statements[None]
         return statement.format(expression)
 
-    def _send_code(self, code, block=True):
-        # execute the code and return the result and HTTP status code
-        self.kernel_client.execute(code)
-        result_found = False
-        iopub_message = None
-        result = None
-        status = 200
-        try:
-            while(result is None):
-                iopub_message = self.kernel_client.get_iopub_msg(block=block, timeout=self.execution_timeout)
-                if iopub_message['header']['msg_type'] == 'execute_result':
-                    result = iopub_message['content']['data']
+    def _process_messages(self, parent_header):
+        idle = False
+        execute_result = None
+        stream_messages = []
+        error_message = None
+        while(not idle):
+            iopub_message = self.kernel_client.get_iopub_msg(block=True, timeout=self.execution_timeout)
+            # Only look at messages which are derived from the parent_header
+            if iopub_message['parent_header']['msg_id'] == parent_header:
+                # On idle status, exit our loop
+                if iopub_message['header']['msg_type'] == 'status' and iopub_message['content']['execution_state'] == 'idle':
+                    idle = True
+                # Store the execute result
+                elif iopub_message['header']['msg_type'] == 'execute_result':
+                    execute_result = iopub_message['content']['data']
+                # Accumulate the stream messages
                 elif iopub_message['header']['msg_type'] == 'stream':
-                    result = iopub_message['content']['text']
+                    stream_messages.append(iopub_message['content']['text'])
+                # Store the error message
                 elif iopub_message['header']['msg_type'] == 'error':
-                    result = 'Error {}: {} \n'.format(
+                    error_message = 'Error {}: {} \n'.format(
                         iopub_message['content']['ename'],
                         iopub_message['content']['evalue']
                     )
-                    status = 500
-        except Empty:
-            access_log.debug('Never found execute result')
-            pass
+
+        if error_message is not None:
+            return True, error_message
+        elif execute_result is not None and execute_result is not '':
+            return False, execute_result
+        else:
+            return False, ''.join(stream_messages)
+
+    def _send_code(self, code):
+        # execute the code and return the result and HTTP status code
+        parent_header = self.kernel_client.execute(code)
+        status = 200
+        error, result = self._process_messages(parent_header=parent_header)
+
+        if error:
+            status = 500
 
         return result, status
 
@@ -70,7 +87,7 @@ class NotebookAPIHandler(tornado.web.RequestHandler):
         request_code = self._request_assignment_for_lang(self.kernel_name, REQUEST)
 
         access_log.debug('Request code for notebook cell is: {}'.format(request_code))
-        self._send_code(request_code, False)
+        self._send_code(request_code)
         result, status = self._send_code(source_code)
 
         # TODO: Will we need to add the ability to specify mime types?
