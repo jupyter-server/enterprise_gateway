3bef7b1148fecbea6ce7a603b18f5ef4fa3481a6
Kevin Bates <kevin.bates@us.ibm.com>
2017-04-25 16:04:48 -0700

    Initial changes for remote kernel support

---
+++ kernel_gateway/services/kernels/remotemanager.py
@@ -0,0 +1,100 @@
+# Copyright (c) Jupyter Development Team.
+# Distributed under the terms of the Modified BSD License.
+"""Kernel managers that operate against a remote process."""
+
+import os
+from ipython_genutils.importstring import import_item
+from .manager import SeedingMappingKernelManager, KernelGatewayIOLoopKernelManager
+
+remote_connection_file_dir = os.getenv('ELYRA_REMOTE_CONNECTION_DIR', '/tmp/')
+
+
+class RemoteMappingKernelManager(SeedingMappingKernelManager):
+    """Extends the SeedingMappingKernelManager. 
+
+    This class is responsible for managing remote kernels. 
+    """
+    remote_process_proxy_class = None
+
+    def _kernel_manager_class_default(self):
+        return 'kernel_gateway.services.kernels.remotemanager.RemoteKernelManager'
+
+    def start_kernel(self, *args, **kwargs):
+        self.log.debug("RemoteMappingKernelManager.start_kernel: {}".format(kwargs['kernel_name']))
+        if kwargs['kernel_name'] is not None:
+            kernel_spec = self.kernel_spec_manager.get_kernel_spec(kwargs['kernel_name'])
+            if self.remote_process_proxy_class is not None:
+                # Detect remote type changes (when changing kernels) and reset remote
+                # process proxy class if necessary.  Note, we need to keep the kernel_manager_class
+                # pointing at this RemoteKernelManager.  If a local kernel is desired, this multi-kernel
+                # manager will do the right thing.
+                #
+                if self.remote_process_proxy_class != kernel_spec.remote_process_proxy_class:
+                    self.log.debug("Detected remote_process_proxy_class change from %s to %s",
+                                   self.remote_process_proxy_class, kernel_spec.remote_process_proxy_class)
+                    self.remote_process_proxy_class = kernel_spec.remote_process_proxy_class
+                    self.log.debug("Kernel manager class is {}".format(self.kernel_manager_class))
+            else:
+                self.remote_process_proxy_class = kernel_spec.remote_process_proxy_class
+        return super(RemoteMappingKernelManager, self).start_kernel(*args, **kwargs)
+
+
+class RemoteKernelManager(KernelGatewayIOLoopKernelManager):
+    """Extends the KernelGatewayIOLoopKernelManager used by the RemoteMappingKernelManager.
+
+    This class is responsible for detecting that a remote kernel is desired, then launching the 
+    appropriate class (previously pulled from the kernel spec).  The remote process 'proxy' is
+    returned - upon which methods of poll(), wait(), send_signal(), and kill() can be called.
+    """
+    remote_connection_file = None
+
+    def format_kernel_cmd(self, extra_arguments=None):
+        """Override for remote kernels so that we can have the kernel command built with a reference to the connection
+           file relative to the destination system, not this local system.  
+        """
+        if self.kernel_spec.remote_process_proxy_class is not None:
+            local_connection_file = self.connection_file
+            self.connection_file = remote_connection_file_dir + os.path.basename(local_connection_file)
+            self.remote_connection_file = self.connection_file
+            cmd = super(RemoteKernelManager, self).format_kernel_cmd(extra_arguments)
+            self.log.debug("Formatted remote kernel cmd is: '{}'".format(cmd))
+            self.connection_file = local_connection_file
+            return cmd
+
+        return super(RemoteKernelManager, self).format_kernel_cmd(extra_arguments)
+
+    def _launch_kernel(self, kernel_cmd, **kw):
+        if self.kernel_spec.remote_process_proxy_class is not None:
+            self.log.debug("Launching remote kernel: {}".format(self.kernel_spec.display_name))
+            remote_process = import_item(self.kernel_spec.remote_process_proxy_class)
+            return remote_process(self, kernel_cmd, **kw)
+
+        return super(RemoteKernelManager, self)._launch_kernel(kernel_cmd, **kw)
+
+    def restart_kernel(self, now=False, **kw):
+        """Need to override and temporarily reset the ip to a local ip to avoid the check about non-local ip usage.
+           The ip will be overwritten with an appropriate ip address when _launch_kernel is called and the process
+           proxy is created.
+        """
+        self.ip = '127.0.0.1'
+
+        super(RemoteKernelManager, self).restart_kernel(now, **kw)
+
+    def signal_kernel(self, signum):
+        """Need to override base method because it tries to send a signal to the (local)
+           process group - so we bypass that code this way.
+        """
+        self.log.debug("RemoteKernelManager.signal_kernel({})".format(signum))
+
+        if self.has_kernel:
+            self.kernel.send_signal(signum)
+        else:
+            raise RuntimeError("Cannot signal kernel. No kernel is running!")
+
+    def cleanup(self, connection_file=True):
+        """Clean up resources when the kernel is shut down"""
+        if self.kernel_spec.remote_process_proxy_class is not None:
+            if connection_file:
+                self.kernel.cleanup()
+
+        return super(RemoteKernelManager, self).cleanup(connection_file)
