6eebf77f359901144166d3e67e3c5ae1e511274f
Christian Kadner <ckadner@us.ibm.com>
2017-05-16 17:34:11 -0700

    Require package "paramiko" (update to PR#1)
    Add "paramiko>=2.1.2" to setup.py and requirements.txt

--- setup.py
+++ setup.py
@@ -58,7 +58,8 @@ containerized and scaled out using common technologies like
         'notebook>=5.0.0,<6.0',
         'traitlets>=4.2.0',
         'tornado>=4.2.0',
-        'requests>=2.7,<3.0'
+        'requests>=2.7,<3.0',
+        'paramiko>=2.1.2'
     ],
     classifiers=[
         'Intended Audience :: Developers',
