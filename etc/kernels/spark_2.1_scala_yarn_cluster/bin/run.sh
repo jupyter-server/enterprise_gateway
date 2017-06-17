f681c7aa13bbf81c9d1e2d9c055b947f1407dc32
Christian Kadner <ckadner@us.ibm.com>
2017-06-16 21:06:54 -0700

    Add Scala kernel files for YARN Cluster mode (Toree)
    Adding 2 new files under etc/kernels/spark_2.1_scala_yarn_cluster/
    - kernel.json (kernel spec)
    - run.sh (calls spark-submit with class org.apache.toree.Main)
    
    Requires Apache Toree to be installed. The kernel.json and run.sh files
    are meant to replace the kernel.json and run.sh files created by the
    Apache Toree installation.

---
+++ etc/kernels/spark_2.1_scala_yarn_cluster/bin/run.sh
@@ -0,0 +1,32 @@
+#!/usr/bin/env bash
+
+echo
+echo "Starting Scala kernel for Spark 2.1 in Yarn Cluster mode as user ${KERNEL_USERNAME:-UNSPECIFIED}"
+echo
+
+if [ -z "${SPARK_HOME}" ]; then
+  echo "SPARK_HOME must be set to the location of a Spark distribution!"
+  exit 1
+fi
+
+PROG_HOME="$(cd "`dirname "$0"`"/..; pwd)"
+KERNEL_ASSEMBLY=`(cd "${PROG_HOME}/lib"; ls -1 toree-assembly-*.jar;)`
+TOREE_ASSEMBLY="${PROG_HOME}/lib/${KERNEL_ASSEMBLY}"
+
+# The SPARK_OPTS values during installation are stored in __TOREE_SPARK_OPTS__. This allows values to be specified during
+# install, but also during runtime. The runtime options take precedence over the install options.
+if [ "${SPARK_OPTS}" = "" ]; then
+   SPARK_OPTS=${__TOREE_SPARK_OPTS__}
+fi
+
+if [ "${TOREE_OPTS}" = "" ]; then
+   TOREE_OPTS=${__TOREE_OPTS__}
+fi
+
+eval exec \
+     "${SPARK_HOME}/bin/spark-submit" \
+     "${SPARK_OPTS}" \
+     --class org.apache.toree.Main \
+     "${TOREE_ASSEMBLY}" \
+     "${TOREE_OPTS}" \
+     "$@"
