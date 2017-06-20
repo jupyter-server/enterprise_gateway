73d4add81611527a62079008b5e0507ee7c0f0a0
akchinSTC <akchin@us.ibm.com>
2017-06-19 21:39:41 -0700

    Add IRkernel files for R support 3 files under etc/kernels/spark_2.1_R_yarn_cluster/

---
+++ etc/kernels/spark_2.1_R_yarn_cluster/run.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+if [ -z "${SPARK_HOME}" ]; then
+  echo "SPARK_HOME must be set to the location of a Spark distribution!"
+  exit 1
+fi
+
+echo
+echo "Starting IRkernel for Spark 2.1 in Yarn Cluster mode as user ${KERNEL_USERNAME:-UNSPECIFIED}"
+echo
+
+PROG_HOME="$(cd "`dirname "$0"`"/; pwd)"
+
+eval exec \
+     "${SPARK_HOME}/bin/spark-submit" \
+     "${SPARK_OPTS}" \
+     "${PROG_HOME}/launch_IRkernel.R" \
+     "$@"
