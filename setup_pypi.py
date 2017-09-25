5b9aee920bcf0dac1b6b5d326b3a1113d773743c
liukun1016 <liukun1016@gmail.com>
2017-09-25 13:49:48 -0700

    Try with PyPI to package

---
+++ setup_pypi.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+  name = 'enterprise_gateway',
+  packages = ['enterprise_gateway'], # this must be the same as the name above
+  version = '0.4',
+  description = 'Jupyter Enterprise Gateway',
+  author = 'STC Elyra team',
+  author_email = 'liukun1016@gmail.com',
+  url = 'http://github.com/liukun1016/enterprise_gateway', # use the URL to the github repo
+  download_url = 'http://github.com/liukun1016/enterprise_gateway/archive/0.4.tar.gz',
+  keywords = ['jupyter', 'gateway', 'enterprise', 'spark'], # arbitrary keywords
+  classifiers = []
+)
