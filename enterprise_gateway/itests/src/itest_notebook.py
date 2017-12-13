e288084699235813b0786f553466149c10d08c17
Kevin Bates <kevin.bates@us.ibm.com>
2017-12-13 10:16:11 -0800

    Validate cell execution results immediately after execution
    With this change, cell execution results are validated immediately
    following the cell's execution.  This allows the output to better
    convey what is going on in the test.  Output is prefixed with `>>> `
    and an indication of whether the execution is considered passed
    or failed is also displayed.
    ```
    --- 1) 1 line(s) of code, 1 output(s)
    print("Hello World")
    >>> Hello World
    Fail ===================================
    Expected output: 'Hello WorlD'
    Actual output:   'Hello World'
    
    --- 2) 2 line(s) of code, 1 output(s)
    print sc.applicationId
    >>> application_1512796058721_0036
    Pass
    ```
    (Note that the 'Hello World' example was forced by manual and temporary
    modification to the notebook file so that failure scenarios could be
    produced.)
    
    The flag `--continue_when_error` is now properly honored.
    
    Some significant refactoring took place to make these changes.  In
    addition, the test now detects kernel restarts and treats those as
    fatal exceptions - moving to the next notebook.
    
    Down the road, we should probably look at refactoring the tests
    further such that each notebook is associated with its own `NotebookTestCase`
    instance (thereby producing more than one test result), and then
    each cell getting equated with its own subTest class.
    
    Fixes #247

--- enterprise_gateway/itests/src/itest_notebook.py
+++ enterprise_gateway/itests/src/itest_notebook.py
@@ -1,7 +1,5 @@
 from unittest import TestCase
 from elyra_client import ElyraClient
-import traceback
-import sys
 
 
 class NotebookTestCase(TestCase):
@@ -13,86 +11,17 @@ class NotebookTestCase(TestCase):
         self.username = kwargs.get("username")
         self.elyra_client = ElyraClient(host=kwargs.get("host"), username=self.username)
         self.enforce_impersonation = kwargs.get("enforce_impersonation")
-
-    @staticmethod
-    def get_assert_code(first_line_code):
-        """
-        Given the first line of the source code, return the code for testing purposes:
-        0 : must be exactly the same, i.e. using assertEqual (default)
-        1 : OK if test output not the same as input, i.e. ignore assert as long as no error
-        2 : must not be the same as each time the execution will definitely be different, then use assertNotEqual
-        3 : Impersonation test, i.e. compare the output username if the self.enforce_impersonation is set as True
-        """
-        if first_line_code:
-            if first_line_code.find("DIFFERENT") > 0:
-                return 1
-            elif first_line_code.find("DEPENDS") > 0:
-                return 2
-            elif first_line_code.find("IMPERSONATION")> 0:
-                return 3
-        return 0
-
-    def execute_codes(self, nb_code_entity):
-        nb_code_entity.kernel_id = self.elyra_client.create_new_kernel(nb_code_entity)
-        self.test_count += 1
-        print("\n{}. {}".format(self.test_count, nb_code_entity))
-        test_code_cell_output_list = None
-        if nb_code_entity.kernel_id:
-            try:
-                test_code_cell_output_list = self.elyra_client.execute_nb_code_entity(nb_code_entity)
-            except Exception as e:
-                print(e.message, traceback.format_exc())
-                if not self.continue_when_error:
-                    print("Failed to execute the codes, now exit.")
-                    sys.exit(-1)
-            finally:
-                self.elyra_client.delete_kernel(nb_code_entity.kernel_id)
-            return test_code_cell_output_list
-
-    def execute_and_assert(self, nb_code_entity):
-        test_code_cell_list = self.execute_codes(nb_code_entity)
-        print("\nFinish execution of codes, now compare/assert")
-        self.assertIsNotNone(test_code_cell_list)
-        self.assertEqual(len(test_code_cell_list), len(nb_code_entity.code_cell_list))
-        index = 0
-        for real_code_cell in nb_code_entity.code_cell_list:
-            if real_code_cell.is_executed() and not real_code_cell.is_output_empty():
-                test_output = test_code_cell_list[index]
-                self.assertIsNotNone(test_output)
-                assert_code = NotebookTestCase.get_assert_code(real_code_cell.get_first_line_code())
-                test_output_str = test_output.seralize_output()
-                real_output_str = real_code_cell.seralize_output()
-                try:
-                    if assert_code != 2:
-                        if assert_code == 0:
-                            self.assertEqual(test_output_str, real_output_str)
-                        elif assert_code == 1:
-                            self.assertNotEqual(test_output_str, real_output_str)
-                        elif assert_code == 3 and self.enforce_impersonation:
-                            # Do impersonation test if and only if the first line is IMPERSONATION (assert code = 3)
-                            # and self.enforce_impersonation is True
-                            test_username = test_output.code_output_list[0].raw_output.get('text')
-                            print("Now doing impersonation test, target username={}, test username={}".format(
-                                self.username, test_username))
-                            self.assertIsNotNone(test_username)
-                            # the raw output is a dict e.g. {'text': 'elyra\r\n', ...}, so here replace the \r\n
-                            test_username = str(test_username).replace("\r\n", "")
-                            self.assertEqual(test_username, self.username)
-                except Exception as e:
-                    print("===================================")
-                    print(e.message)
-                    print(traceback.format_exc())
-                    print("{}\nExecute count {}".format(nb_code_entity, real_code_cell.execute_count))
-                    print("[Test output]\n", test_output_str)
-                    print("[Real output]\n", real_output_str)
-                    if not self.continue_when_error:
-                        sys.exit(-1)
-            index += 1
-        print("Testing completed: {}".format(nb_code_entity))
+        print("NotebookTestCase arguments: {}\n".format(kwargs))
 
     def test_kernels_batch(self):
-        self.test_count = 0
-        print("Begin testing batch of {} notebook(s)...\n".format(len(self.nb_entities_list)))
+        test_count = 1
+        errors = 0
+        print("Begin testing batch of {} notebook(s)...".format(len(self.nb_entities_list)))
         for nb_code_entity in self.nb_entities_list:
-            self.execute_and_assert(nb_code_entity)
+            try:
+                errors = errors + nb_code_entity.test_notebook(test_count, self)
+            except Exception as e:
+                errors = errors + 1
+            test_count = test_count + 1
         print("\nBatch completed.")
+        self.assertEquals(0, errors)
