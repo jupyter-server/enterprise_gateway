6711f4d5fe2855b8cb92c5a656208d4fcd835ad5
Kevin Bates <kbates4@gmail.com>
2018-05-09 15:56:02 -0700

    Detect missing jar files during scala kernel launch
    Because scala kernel launch is dependent upon the separately installed
    Toree jar file, this PR adds a check to ensure that file exists and, if
    not, echos an error message and exits the script.  While at it, we also
    ensure that the toree kernel launcher jar is present as well.
    
    Since there isn't a very graceful way to communicate this issue, the
    changes also include some improved log statements instructing where
    users should look to gain details regarding the launch failure.
    
    Fixes #327
    Closes #337

--- etc/kernelspecs/spark_scala_yarn_client/bin/run.sh
+++ etc/kernelspecs/spark_scala_yarn_client/bin/run.sh
@@ -20,6 +20,10 @@ fi
 PROG_HOME="$(cd "`dirname "$0"`"/..; pwd)"
 KERNEL_ASSEMBLY=`(cd "${PROG_HOME}/lib"; ls -1 toree-assembly-*.jar;)`
 TOREE_ASSEMBLY="${PROG_HOME}/lib/${KERNEL_ASSEMBLY}"
+if [ ! -f ${TOREE_ASSEMBLY} ]; then
+    echo "Toree assembly '${PROG_HOME}/lib/toree-assembly-*.jar' is missing.  Exiting..."
+    exit 1
+fi
 
 # The SPARK_OPTS values during installation are stored in __TOREE_SPARK_OPTS__. This allows values to be specified during
 # install, but also during runtime. The runtime options take precedence over the install options.
@@ -36,6 +40,10 @@ JARS="${TOREE_ASSEMBLY}"
 # Toree launcher app path
 LAUNCHER_JAR=`(cd "${PROG_HOME}/lib"; ls -1 toree-launcher*.jar;)`
 LAUNCHER_APP="${PROG_HOME}/lib/${LAUNCHER_JAR}"
+if [ ! -f ${LAUNCHER_APP} ]; then
+    echo "Scala launcher jar '${PROG_HOME}/lib/toree-launcher*.jar' is missing.  Exiting..."
+    exit 1
+fi
 
 set -x
 eval exec "${IMPERSONATION_OPTS}" \
