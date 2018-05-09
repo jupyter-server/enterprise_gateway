347e8bccc54d32d99f0f80874ecb970cc6685eff
charlieeeeeee <charlie@ca.ibm.com>
2018-05-09 18:04:33 -0400

    Add R support for Conductor.

---
+++ etc/kernelspecs/spark_R_conductor_cluster/bin/run.sh
@@ -0,0 +1,32 @@
+#!/usr/bin/env bash
+
+if [ "${EG_IMPERSONATION_ENABLED}" = "True" ]; then
+        IMPERSONATION_OPTS="--proxy-user ${KERNEL_USERNAME:-UNSPECIFIED}"
+        USER_CLAUSE="as user ${KERNEL_USERNAME:-UNSPECIFIED}"
+else
+        IMPERSONATION_OPTS=""
+        USER_CLAUSE="on behalf of user ${KERNEL_USERNAME:-UNSPECIFIED}"
+fi
+
+echo
+echo "Starting IRkernel for Spark Cluster mode ${USER_CLAUSE}"
+echo
+
+if [ -z "${SPARK_HOME}" ]; then
+  echo "SPARK_HOME must be set to the location of a Spark distribution!"
+  exit 1
+fi
+
+PROG_HOME="$(cd "`dirname "$0"`"/..; pwd)"
+
+# Add gateway_listener.py to files for spark-opts
+ADDITIONAL_OPTS="--files ${PROG_HOME}/scripts/gateway_listener.py"
+
+eval exec \
+     "${SPARK_HOME}/bin/spark-submit" \
+     "${SPARK_OPTS}" \
+     "${ADDITIONAL_OPTS}" \
+     "${IMPERSONATION_OPTS}" \
+     "${PROG_HOME}/scripts/launch_IRkernel.R" \
+     "${LAUNCH_OPTS}" \
+     "$@"
