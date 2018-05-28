ac25ba39cf9d5fe136006a9e6c80fb0b09e85500
Luciano Resende <lresende@apache.org>
2018-05-27 21:07:08 -0700

    Prepare for next development iteration

--- enterprise_gateway/_version.py
+++ enterprise_gateway/_version.py
@@ -4,9 +4,10 @@
 # Distributed under the terms of the Modified BSD License.
 
 version_info = (
+    1,
     0,
-    9,
-    1
+    0,
+    'dev0'
 )
 
 __version__ = '.'.join(map(str, version_info))
