193b17e322cb7542cdc6bc3ccb0b53d0f32d457e
Atin Sood <asood@us.ibm.com>
2018-07-17 15:56:09 -0400

    additional changes fixing incorrect path to kernel specs

--- etc/docker/kubernetes-kernel-tf-py-gpu/bootstrap-kernel.sh
+++ etc/docker/kubernetes-kernel-tf-py-gpu/bootstrap-kernel.sh
@@ -4,8 +4,8 @@ echo kernel-bootstrap.sh: language=${KERNEL_LANGUAGE}, connection-file=${KERNEL_
 
 if [[ "${KERNEL_LANGUAGE}" == "python" ]];
 then
-	echo "python /usr/local/share/jupyter/kernels/python_kubernetes/scripts/launch_ipykernel.py ${KERNEL_CONNECTION_FILENAME} --RemoteProcessProxy.response-address ${EG_RESPONSE_ADDRESS} --RemoteProcessProxy.spark-context-initialization-mode ${KERNEL_SPARK_CONTEXT_INIT_MODE}"
-	python /usr/local/share/jupyter/kernels/python_kubernetes/scripts/launch_ipykernel.py ${KERNEL_CONNECTION_FILENAME} --RemoteProcessProxy.response-address ${EG_RESPONSE_ADDRESS} --RemoteProcessProxy.spark-context-initialization-mode ${KERNEL_SPARK_CONTEXT_INIT_MODE}
+	echo "python /usr/local/share/jupyter/kernels/python_tf_gpu_kubernetes/scripts/launch_ipykernel.py ${KERNEL_CONNECTION_FILENAME} --RemoteProcessProxy.response-address ${EG_RESPONSE_ADDRESS} --RemoteProcessProxy.spark-context-initialization-mode ${KERNEL_SPARK_CONTEXT_INIT_MODE}"
+	python /usr/local/share/jupyter/kernels/python_tf_gpu_kubernetes/scripts/launch_ipykernel.py ${KERNEL_CONNECTION_FILENAME} --RemoteProcessProxy.response-address ${EG_RESPONSE_ADDRESS} --RemoteProcessProxy.spark-context-initialization-mode ${KERNEL_SPARK_CONTEXT_INIT_MODE}
 else
 	echo "Unrecognized value for KERNEL_LANGUAGE: '${KERNEL_LANGUAGE}'!"
 	exit 1
