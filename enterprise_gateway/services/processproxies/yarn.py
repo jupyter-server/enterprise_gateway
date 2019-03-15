8e7c00c707052754134dcfd46e48905dbfbc6d76
Kevin Bates <kbates4@gmail.com>
2019-03-15 14:38:12 -0700

    Increase shutdown wait time for YARN kernels

--- enterprise_gateway/services/processproxies/yarn.py
+++ enterprise_gateway/services/processproxies/yarn.py
@@ -26,6 +26,7 @@ logging.getLogger('urllib3.connectionpool').setLevel(os.environ.get('EG_YARN_LOG
 local_ip = localinterfaces.public_ips()[0]
 poll_interval = float(os.getenv('EG_POLL_INTERVAL', '0.5'))
 max_poll_attempts = int(os.getenv('EG_MAX_POLL_ATTEMPTS', '10'))
+yarn_shutdown_wait_time = float(os.getenv('EG_YARN_SHUTDOWN_WAIT_TIME', '15.0'))
 
 
 class YarnClusterProcessProxy(RemoteProcessProxy):
@@ -52,6 +53,15 @@ class YarnClusterProcessProxy(RemoteProcessProxy):
         else:
             self.resource_mgr = ResourceManager(address=yarn_master,
                                                 port=yarn_port)
+
+        # YARN applications tend to take longer than the default 5 second wait time.  Rather than
+        # require a command-line option for those using YARN, we'll adjust based on a local env that
+        # defaults to 15 seconds.  Note: we'll only adjust if the current wait time is shorter than
+        # the desired value.
+        if kernel_manager.shutdown_wait_time < yarn_shutdown_wait_time:
+            kernel_manager.shutdown_wait_time = yarn_shutdown_wait_time
+            self.log.debug("{class_name} shutdown wait time adjusted to {wait_time} seconds.".
+                           format(class_name=type(self).__name__, wait_time=kernel_manager.shutdown_wait_time))
 
     def launch_process(self, kernel_cmd, **kwargs):
         """Launches the specified process within a YARN cluster environment."""
