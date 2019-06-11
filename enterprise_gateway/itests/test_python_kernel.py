7586bf49fc5b22d51c1702c506f3d5555899c8b2
Kevin Bates <kbates4@gmail.com>
2019-06-11 10:18:48 -0700

    Fix interrupts on intensive operations
    When performing CPU-intensive operations within a cell, remote python
    kernels can have their interrupt capabilities stop working due to
    thread starvation.  This is because the gateway-listener thread is
    unable to signal the kernel.  This change uses a `Process` rather
    than a `Thread` in which to run the gateway-listener - which results
    in it not being starved.  As a result, the signal can be sent to the
    kernel and the cell's operation interrupted.
    
    Fixes #682

--- enterprise_gateway/itests/test_python_kernel.py
+++ enterprise_gateway/itests/test_python_kernel.py
@@ -50,10 +50,11 @@ class PythonKernelBaseTestCase(TestBase):
 
         # Build the code list to interrupt, in this case, its a sleep call.
         interrupted_code = list()
-        interrupted_code.append("import time\n")
-        interrupted_code.append("print('begin')\n")
-        interrupted_code.append("time.sleep(30)\n")
-        interrupted_code.append("print('end')\n")
+        interrupted_code.append("i = 2\n")
+        interrupted_code.append("while i > 0:\n")
+        interrupted_code.append("    i *= i\n")
+        interrupted_code.append("    print(i)\n")
+
         interrupted_result = self.kernel.execute(interrupted_code)
 
         # Ensure the result indicates an interrupt occurred
