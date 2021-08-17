2bedb41a11d889e6214de9c51d56cdbae6c1dbba
忆梵 <sunyifan.syf@alibaba-inc.com>
2021-08-17 19:11:45 +0800

    1. use k8s process proxy as base class; 2. opt kernel.json

--- enterprise_gateway/services/processproxies/crd.py
+++ enterprise_gateway/services/processproxies/crd.py
@@ -2,64 +2,26 @@
 # Distributed under the terms of the Modified BSD License.
 """Code related to managing kernels running based on k8s custom resource."""
 
-import abc
-import signal
-import urllib3  # docker ends up using this and it causes lots of noise, so turn off warnings
-
-from jupyter_client import localinterfaces
-
-from .processproxy import RemoteProcessProxy
-from kubernetes import config, client
-
-urllib3.disable_warnings()
-local_ip = localinterfaces.public_ips()[0]
-config.load_incluster_config()
+from .k8s import KubernetesProcessProxy
+from kubernetes import client
 
 
-class CustomResourceProxy(RemoteProcessProxy):
+class CustomResourceProxy(KubernetesProcessProxy):
     group = version = plural = None
     custom_resource_template_name = None
     kernel_resource_name = None
 
     def __init__(self, kernel_manager, proxy_config):
         super(CustomResourceProxy, self).__init__(kernel_manager, proxy_config)
-        self.assigned_node_ip = None
 
     async def launch_process(self, kernel_cmd, **kwargs):
-        kwargs['env']['KERNEL_CRD_NAME'] = self.kernel_resource_name
+        kwargs['env']['KERNEL_RESOURCE_NAME'] = self.kernel_resource_name = self._determine_kernel_pod_name(**kwargs)
         kwargs['env']['KERNEL_CRD_GROUP'] = self.group
         kwargs['env']['KERNEL_CRD_VERSION'] = self.version
         kwargs['env']['KERNEL_CRD_PLURAL'] = self.plural
 
         await super(CustomResourceProxy, self).launch_process(kernel_cmd, **kwargs)
-
-        self.local_proc = self.launch_kernel(kernel_cmd, **kwargs)
-        self.pid = self.local_proc.pid
-        self.ip = local_ip
-
-        self.log.info("{}: kernel launched. KernelID: {}, cmd: '{}'"
-                      .format(self.__class__.__name__, self.kernel_id, kernel_cmd))
-
-        await self.confirm_remote_startup()
         return self
-
-    def poll(self):
-        result = False
-
-        resource_status = self.get_resource_status(None)
-
-        if resource_status in self.get_initial_states():
-            result = None
-
-        return result
-
-    def send_signal(self, signum):
-        if signum == 0:
-            return self.poll()
-        elif signum == signal.SIGKILL:
-            return self.kill()
-        else:
-            return super(CustomResourceProxy, self).send_signal(signum)
 
     def kill(self):
         result = None
@@ -68,45 +30,6 @@ class CustomResourceProxy(RemoteProcessProxy):
             result = self.terminate_custom_resource()
 
         return result
-
-    def cleanup(self):
-        # Since container objects don't necessarily go away on their own, we need to perform the same
-        # cleanup we'd normally perform on forced kill situations.
-
-        self.kill()
-        super(CustomResourceProxy, self).cleanup()
-
-    async def confirm_remote_startup(self):
-        self.start_time = RemoteProcessProxy.get_current_time()
-        i = 0
-        ready_to_connect = False  # we're ready to connect when we have a connection file to use
-        while not ready_to_connect:
-            i += 1
-            await self.handle_timeout()
-
-            resource_status = self.get_resource_status(str(i))
-            if resource_status and self.assigned_host != '':
-                ready_to_connect = await self.receive_connection_info()
-                self.pid = self.pgid = 0  # We won't send process signals for kubernetes lifecycle management
-            else:
-                self.detect_launch_failure()
-
-    def get_process_info(self):
-        process_info = super(CustomResourceProxy, self).get_process_info()
-        process_info.update({'assigned_node_ip': self.assigned_node_ip, })
-        return process_info
-
-    def load_process_info(self, process_info):
-        super(CustomResourceProxy, self).load_process_info(process_info)
-        self.assigned_node_ip = process_info['assigned_node_ip']
-
-    @abc.abstractmethod
-    def get_initial_states(self):
-        raise NotImplementedError
-
-    @abc.abstractmethod
-    def get_resource_status(self, iteration):
-        raise NotImplementedError
 
     def terminate_custom_resource(self):
         try:
--- enterprise_gateway/services/processproxies/spark_operator.py
+++ enterprise_gateway/services/processproxies/spark_operator.py
@@ -2,11 +2,7 @@
 # Distributed under the terms of the Modified BSD License.
 
 import os
-import re
-from kubernetes import client
-
-from .crd import CustomResourceProxy
-from ..sessions.kernelsessionmanager import KernelSessionManager
+from .crd import CustomResourceProxy, client
 
 enterprise_gateway_namespace = os.environ.get('EG_NAMESPACE', 'default')
 
@@ -14,36 +10,27 @@ enterprise_gateway_namespace = os.environ.get('EG_NAMESPACE', 'default')
 class SparkOperatorProcessProxy(CustomResourceProxy):
     def __init__(self, kernel_manager, proxy_config):
         super(SparkOperatorProcessProxy, self).__init__(kernel_manager, proxy_config)
-        self.kernel_namespace = None
         self.group = 'sparkoperator.k8s.io'
         self.version = 'v1beta2'
         self.plural = 'sparkapplications'
 
-    async def launch_process(self, kernel_cmd, **kwargs):
-        kwargs['env'] = dict(os.environ, **kwargs['env'])  # FIXME: Should probably use process-whitelist in JKG #280
-        self.kernel_resource_name = self._determine_kernel_resource_name(**kwargs)
-        self.kernel_namespace = self._determine_kernel_namespace(**kwargs)  # will create namespace if not provided
-
-        await super(SparkOperatorProcessProxy, self).launch_process(kernel_cmd, **kwargs)
-        return self
-
-    def get_resource_status(self, iteration):
-        resource_status = None
+    def get_container_status(self, iteration):
+        pod_status = pod_info = None
 
         try:
-            custom_resource = client.CustomObjectsApi().get_namespaced_custom_object(self.group, self.version,
-                                                                                     self.kernel_namespace,
-                                                                                     self.plural,
-                                                                                     self.kernel_resource_name)
+            custom_resource = client.CustomObjectsApi().get_namespaced_custom_object(
+                self.group,
+                self.version,
+                self.kernel_namespace,
+                self.plural,
+                self.kernel_resource_name
+            )
 
             if custom_resource:
-                resource_status = custom_resource['status']['applicationState']['state']
                 pod_name = custom_resource['status']['driverInfo']['podName']
                 pod_info = client.CoreV1Api().read_namespaced_pod(pod_name, self.kernel_namespace)
-            else:
-                pod_info = None
         except Exception:
-            pod_info = None
+            pass
 
         if pod_info and pod_info.status:
             pod_status = pod_info.status.phase
@@ -52,35 +39,4 @@ class SparkOperatorProcessProxy(CustomResourceProxy):
                 self.assigned_host = pod_info.metadata.name
                 self.assigned_node_ip = pod_info.status.host_ip
 
-        if iteration:
-            self.log.debug("{}: Waiting to connect to k8s custom resource in namespace '{}'. "
-                           "Name: '{}', Status: '{}', Pod IP: '{}', KernelID: '{}'".
-                           format(iteration, self.kernel_namespace, self.kernel_resource_name, resource_status,
-                                  self.assigned_ip, self.kernel_id))
-
-        return resource_status
-
-    def get_initial_states(self):
-        return {'SUBMITTED', 'RUNNING'}
-
-    def _determine_kernel_resource_name(self, **kwargs):
-        resource_name = kwargs['env'].get('KERNEL_RESOURCE_NAME',
-                                          KernelSessionManager.get_kernel_username(**kwargs) + '-' + self.kernel_id)
-
-        resource_name = re.sub('[^0-9a-z]+', '-', resource_name.lower())
-        while resource_name.startswith('-'):
-            resource_name = resource_name[1:]
-        while resource_name.endswith('-'):
-            resource_name = resource_name[:-1]
-        kwargs['env']['KERNEL_RESOURCE_NAME'] = resource_name
-
-        return resource_name
-
-    def _determine_kernel_namespace(self, **kwargs):
-        namespace = kwargs['env'].get('KERNEL_NAMESPACE')
-        if namespace is None:
-            kwargs['env']['KERNEL_NAMESPACE'] = enterprise_gateway_namespace
-        else:
-            self.log.info("KERNEL_NAMESPACE provided by client: {}".format(namespace))
-
-        return namespace
+        return pod_status
