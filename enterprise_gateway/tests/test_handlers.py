4ebcd4357f8f630d6e29b08cec5615cd5e8a5208
Kevin Bates <kbates4@gmail.com>
2022-01-24 14:24:59 -0800

    Extend attempts for kernel_info_reply, print msg for debug

--- enterprise_gateway/tests/test_handlers.py
+++ enterprise_gateway/tests/test_handlers.py
@@ -395,9 +395,10 @@ class TestDefaults(TestHandlers):
         }))
 
         # Assert the reply comes back. Test will timeout if this hangs.
-        for _ in range(4):
+        for _ in range(8):
             msg = yield ws.read_message()
             msg = json_decode(msg)
+            print(f"test_kernel_comm: {_}: {msg['msg_type']}")
             if(msg['msg_type'] == 'kernel_info_reply'):
                 break
         else:
