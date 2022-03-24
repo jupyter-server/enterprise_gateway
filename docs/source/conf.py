853466a7e639a9ba97b4f8944b5727a233cef655
Steven Silvester <steven.silvester@ieee.org>
2022-03-24 11:29:26 -0500

    clean up CI (#1052)

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -11,9 +11,6 @@
 
 import os
 
-# Needed for conversion from markdown to html
-import recommonmark.parser
-
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
