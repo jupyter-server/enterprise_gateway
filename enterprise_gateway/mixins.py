716751e0b883d48e207c512373ebc1af74c67a4b
李超 <2637691052@qq.com>
2022-04-06 20:10:08 +0800

    Add a kernel opening mechanism for load balancing!
    Add a kernel opening mechanism for load balancing.Turn on at the node with the least number of cores.

--- enterprise_gateway/mixins.py
+++ enterprise_gateway/mixins.py
@@ -94,7 +94,7 @@ class TokenAuthorizationMixin:
             if client_token is None:
                 client_token = self.request.headers.get("Authorization")
                 if client_token and client_token.startswith(self.header_prefix):
-                    client_token = client_token[self.header_prefix_len :]
+                    client_token = client_token[self.header_prefix_len:]
                 else:
                     client_token = None
             if client_token != server_token:
@@ -375,8 +375,8 @@ class EnterpriseGatewayConfigMixin(Configurable):
     @default("list_kernels")
     def list_kernels_default(self):
         return (
-            os.getenv(self.list_kernels_env, os.getenv("KG_LIST_KERNELS", "False")).lower()
-            == "true"
+                os.getenv(self.list_kernels_env, os.getenv("KG_LIST_KERNELS", "False")).lower()
+                == "true"
         )
 
     env_whitelist_env = "EG_ENV_WHITELIST"
@@ -430,6 +430,19 @@ class EnterpriseGatewayConfigMixin(Configurable):
     @default("remote_hosts")
     def remote_hosts_default(self):
         return os.getenv(self.remote_hosts_env, self.remote_hosts_default_value).split(",")
+
+    # load balance
+    load_balance_env = "EG_LOAD_BALANCE"
+    load_balance_default_value = False
+    load_balance = Bool(
+        load_balance_default_value,
+        config=True,
+        help="""DistributedProcessProxy use load balance open kernel by host's kernel numbers"""
+    )
+
+    @default("load_balance")
+    def load_balance_default(self):
+        return bool(os.getenv(self.load_balance_env, self.load_balance_default_value))
 
     # Yarn endpoint
     yarn_endpoint_env = "EG_YARN_ENDPOINT"
