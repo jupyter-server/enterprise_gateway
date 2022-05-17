53daa2de3111452facea0f4764fbf9c315704913
Steven Silvester <steven.silvester@ieee.org>
2022-05-17 14:11:41 -0500

    cleanup tbump command

--- release.sh
+++ release.sh
@@ -195,7 +195,7 @@ function update_version_to_release {
 
     # Update tbump-managed versions
     pip install tbump
-    tbump --non-interactive $RELEASE_VERSION
+    tbump --non-interactive --no-tag --no-push $RELEASE_VERSION
 
     # Update Kubernetes deployment descriptor
     sed -i .bak "s@elyra/enterprise-gateway:dev@elyra/enterprise-gateway:$RELEASE_VERSION@g" etc/kubernetes/enterprise-gateway.yaml
@@ -219,7 +219,7 @@ function update_version_to_development {
 
     # Update tbump-managed versions
     pip install tbump
-    tbump --non-interactive $RELEASE_VERSION
+    tbump --non-interactive --no-tag --no-push $RELEASE_VERSION
 
     # Update Kubernetes deployment descriptor
     sed -i .bak "s@elyra/enterprise-gateway:$RELEASE_VERSION@elyra/enterprise-gateway:dev@g" etc/kubernetes/enterprise-gateway.yaml
