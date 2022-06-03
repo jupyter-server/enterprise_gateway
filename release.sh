dfbaeea2f05a96522344e840b3358cc6e1861178
Kevin Bates <kbates4@gmail.com>
2022-06-03 10:45:26 -0700

    Release preparation (#1102)
    * Properly handle pre-release versions for helm version field
    
    Helm uses a strict semver semantic for its 'version' field.  This
    change injects a hyphen separator between the release version and
    the pre-release (or dev) tag to avoid helm lint failures.
    
    * Normalize image tag to avoid release build failures

--- release.sh
+++ release.sh
@@ -202,7 +202,10 @@ function update_version_to_release {
     sed -i .bak "s@elyra/kernel-image-puller:dev@elyra/kernel-image-puller:$RELEASE_VERSION@g" etc/kubernetes/enterprise-gateway.yaml
 
     # Update Kubernetes Helm chart and values files (tbump will handle appVersion in Chart.yaml)
-    sed -i .bak "s@version: [0-9,\.,a-z]*@version: $RELEASE_VERSION@g" etc/kubernetes/helm/enterprise-gateway/Chart.yaml
+    # We need to inject "-" prior to pre-release suffices for 'version:' since it follows strict semantic version rules.
+    # For example 3.0.0rc1 -> 3.0.0-rc1
+    k8s_version=`echo $RELEASE_VERSION | sed 's/\([0-9]\)\([a-z]\)/\1-\2/'`
+    sed -i .bak "s@version: [0-9,\.,a-z]*@version: $k8s_version@g" etc/kubernetes/helm/enterprise-gateway/Chart.yaml
 
     sed -i .bak "s@elyra/enterprise-gateway:dev@elyra/enterprise-gateway:$RELEASE_VERSION@g" etc/kubernetes/helm/enterprise-gateway/values.yaml
     sed -i .bak "s@elyra/kernel-image-puller:dev@elyra/kernel-image-puller:$RELEASE_VERSION@g" etc/kubernetes/helm/enterprise-gateway/values.yaml
@@ -212,7 +215,7 @@ function update_version_to_release {
     sed -i .bak "s@elyra/kernel-image-puller:dev@elyra/kernel-image-puller:$RELEASE_VERSION@g" etc/docker/docker-compose.yml
 
     # Update documentation - this is a one-way change since links will not be valid in dev "releases".
-    find docs/source/*.md -type f -exec sed -i .bak "s@$PREVIOUS_VERSION@$RELEASE_VERSION@g" {} \;
+    find docs/source -name "*.md" -type f -exec sed -i .bak "s@$PREVIOUS_VERSION@$RELEASE_VERSION@g" {} \;
 }
 
 function update_version_to_development {
@@ -227,8 +230,9 @@ function update_version_to_development {
     sed -i .bak "s@elyra/kernel-image-puller:$RELEASE_VERSION@elyra/kernel-image-puller:dev@g" etc/kubernetes/enterprise-gateway.yaml
 
     # Update Kubernetes Helm chart and values files (tbump will handle appVersion in Chart.yaml)
-    # We need to strip off any ".devN" suffix from the dev version for 'version:' since it must follow strict semantic version rules.
-    k8s_version=`echo $DEVELOPMENT_VERSION | sed 's/\.dev.//'`
+    # We need to replace ".devN" suffix with "-devN for 'version:' since it follows strict semantic version rules.
+    # For example 3.0.0.dev1 -> 3.0.0-dev1
+    k8s_version=`echo $DEVELOPMENT_VERSION | sed 's/\.\(dev\)/-\1/'`
     sed -i .bak "s@version: [0-9,\.,a-z]*@version: $k8s_version@g" etc/kubernetes/helm/enterprise-gateway/Chart.yaml
 
     sed -i .bak "s@elyra/enterprise-gateway:$RELEASE_VERSION@elyra/enterprise-gateway:dev@g" etc/kubernetes/helm/enterprise-gateway/values.yaml
