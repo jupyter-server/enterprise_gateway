0f152b221d3b7ccaaf7818c8183128bd48e459a5
Luciano Resende <lresende@apache.org>
2022-06-13 16:29:56 -0700

    Remove unused file

--- update-demo-base.sh
+++
@@ -1,3 +0,0 @@
-make clean dist clean-demo-base demo-base push-demo-base clean-enterprise-gateway-demo enterprise-gateway-demo push-enterprise-gateway-demo
-
-# make clean dist clean-demo-base demo-base clean-enterprise-gateway-demo enterprise-gateway-demo
