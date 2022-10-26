b2ca2d7e664edd34947d78134d70745989171b30
blackmetal23 <blackmetal23@users.noreply.github.com>
2022-10-27 01:17:32 +0200

    Update etc/kernel-launchers/kubernetes/scripts/launch_kubernetes.py
    Co-authored-by: Kevin Bates <kbates4@gmail.com>

--- etc/kernel-launchers/kubernetes/scripts/launch_kubernetes.py
+++ etc/kernel-launchers/kubernetes/scripts/launch_kubernetes.py
@@ -162,7 +162,7 @@ def launch_kubernetes_kernel(
             elif k8s_obj["kind"] == "ConfigMap":
                 if pod_template_file is None:
                     if pod_created is not None:
-                        # Create link dependance between pod and configmap, usefull to delete service when kernel stop
+                        # Create dependency between pod and configmap, useful to delete service when kernel stops
                         k8s_obj["metadata"]["ownerReferences"] = [
                             {
                                 "apiVersion": "v1",
