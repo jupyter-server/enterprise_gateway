031cc28da73bc8a0957732cdaee9b7d459f290f7
Steven Silvester <steven.silvester@ieee.org>
2022-12-22 18:54:56 -0600

    lint

--- enterprise_gateway/__init__.py
+++ enterprise_gateway/__init__.py
@@ -1,8 +1,7 @@
+"""Lazy-loading entrypoint for the enterprise gateway package."""
 # Copyright (c) Jupyter Development Team.
 # Distributed under the terms of the Modified BSD License.
 from ._version import __version__  # noqa
-
-"""Lazy-loading entrypoint for the enterprise gateway package."""
 
 
 def launch_instance(*args, **kwargs):
