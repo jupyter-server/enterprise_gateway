e32f624954d9b04f96c790fd0a45bbcdd6c00766
pq43 <51523958+pq43@users.noreply.github.com>
2023-02-22 21:32:57 -0800

    Fix linting :)

--- enterprise_gateway/client/gateway_client.py
+++ enterprise_gateway/client/gateway_client.py
@@ -35,12 +35,14 @@ class GatewayClient:
             f"https://{host}/api/kernels" if use_secure_connection else f"http://{host}/api/kernels"
         )
         self.ws_api_endpoint = (
-            f"wss://{host}/api/kernels" if use_secure_connection else f"ws://{host}/api/kernels"
+            f"wss://{host}/api/kernels" if use_secure_connection else f"wss://{host}/api/kernels"
         )
         self.log = logging.getLogger("GatewayClient")
         self.log.setLevel(log_level)
 
-    def start_kernel(self, kernelspec_name, extra_env, username=DEFAULT_USERNAME, timeout=REQUEST_TIMEOUT):
+    def start_kernel(
+        self, kernelspec_name, extra_env, username=DEFAULT_USERNAME, timeout=REQUEST_TIMEOUT
+    ):
         """Start a kernel."""
         self.log.info(f"Starting a {kernelspec_name} kernel ....")
 
