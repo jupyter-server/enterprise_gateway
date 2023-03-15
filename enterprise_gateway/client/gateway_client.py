f72cca89667954997f8707d5203a1f08f5463bf6
Nikhil Mehta <nikhil_mehta2@apple.com>
2023-03-14 17:24:36 -0700

    Initialize variables before websocket.create_connection in KernelClient.__init__()

--- enterprise_gateway/client/gateway_client.py
+++ enterprise_gateway/client/gateway_client.py
@@ -109,38 +109,37 @@ class KernelClient:
         self.kernel_id = kernel_id
         self.log = logger
         self.log.debug(f"Initializing kernel client ({kernel_id}) to {self.kernel_ws_api_endpoint}")
+        self.response_queues = {}
+        # startup reader thread
+
+        self.response_reader = Thread(target=self._read_responses)
+        self.response_reader.start()
+        self.interrupt_thread = None
+        self.kernel_socket = None
 
         try:
             self.kernel_socket = websocket.create_connection(
                 f"{ws_api_endpoint}/{kernel_id}/channels", timeout=timeout, enable_multithread=True
             )
         except Exception as e:
-            self.kernel_socket = None
             self.log.error(e)
             self.shutdown()
             raise e
-
-        self.response_queues = {}
-
-        # startup reader thread
-        self.response_reader = Thread(target=self._read_responses)
-        self.response_reader.start()
-        self.interrupt_thread = None
 
     def shutdown(self):
         """Shut down the client."""
         # Terminate thread, close socket and clear queues.
         self.shutting_down = True
 
-        if hasattr(self, 'kernel_socket') and self.kernel_socket:
+        if self.kernel_socket:
             self.kernel_socket.close()
             self.kernel_socket = None
 
-        if hasattr(self, 'response_queues') and self.response_queues:
+        if self.response_queues:
             self.response_queues.clear()
             self.response_queues = None
 
-        if hasattr(self, 'response_reader') and self.response_reader:
+        if self.response_reader:
             self.response_reader.join(timeout=2.0)
             if self.response_reader.is_alive():
                 self.log.warning("Response reader thread is not terminated, continuing...")
